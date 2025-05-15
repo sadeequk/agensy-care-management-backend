6b3132e72179ab3787aa96a51af025db65a44bf1
sadeeq <sadeequk8@gmail.com>
2025-05-15 14:58:03 +0500

    📌 delete api added

--- routes/client.contact.routes.js
+++ routes/client.contact.routes.js
@@ -4,11 +4,11 @@ const clientContactController = require("../controllers/client.contact.controlle
 const verifyCognitoToken = require("../middlewares/auth.mw");
 
 router.post("/:id", verifyCognitoToken, clientContactController.contact_post); //client id
-router.get("/single/:id", verifyCognitoToken, clientContactController.contact_get); //contact id
-router.get("/all/:id", verifyCognitoToken, clientContactController.client_contacts_get); //client id
+router.get("/single-contact/:id", verifyCognitoToken, clientContactController.contact_get); //contact id
+router.get("/all-by-client/:id", verifyCognitoToken, clientContactController.client_contacts_get); //client id
 router.put("/:id", verifyCognitoToken, clientContactController.contact_put); //contact id
+router.delete("/:id", verifyCognitoToken, clientContactController.contact_delete); //contact id
 
 // router.put("/status/:id", verifyCognitoToken, clientContactController.status_put); //contact id
-// router.delete("/:id", verifyCognitoToken, clientContactController.contact_delete); //contact id
 
 module.exports = router;
