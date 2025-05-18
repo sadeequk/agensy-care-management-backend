6f1aec5f59fb86a6d65677eca5ba7fb4d9b85640
sadeeq <sadeequk8@gmail.com>
2025-05-18 23:06:15 +0500

    📌 allow null in notes

--- validation/healthcare.provider.schemas.js
+++ validation/healthcare.provider.schemas.js
@@ -3,26 +3,26 @@ const Joi = require("joi");
 exports.provider_post = Joi.object({
   provider_type: Joi.string().required().max(100),
   provider_name: Joi.string().required().max(255),
-  specialty: Joi.string().max(255).required(),
-  address: Joi.string().max(255).required(),
-  phone: Joi.string().max(20).required(),
+  specialty: Joi.string().max(255),
+  address: Joi.string().max(255),
+  phone: Joi.string().max(20),
   fax: Joi.string().max(20),
-  last_visit: Joi.date().iso().required(),
-  next_visit: Joi.date().iso().required(),
-  notes: Joi.string(),
+  last_visit: Joi.date().iso(),
+  next_visit: Joi.date().iso(),
+  notes: Joi.string().allow(""),
   active: Joi.boolean().default(true),
 });
 
 exports.provider_put = Joi.object({
   provider_type: Joi.string().required().max(100),
   provider_name: Joi.string().required().max(255),
-  specialty: Joi.string().max(255).required(),
-  address: Joi.string().max(255).required(),
-  phone: Joi.string().max(20).required(),
+  specialty: Joi.string().max(255),
+  address: Joi.string().max(255),
+  phone: Joi.string().max(20),
   fax: Joi.string().max(20),
-  last_visit: Joi.date().iso().required(),
-  next_visit: Joi.date().iso().required(),
-  notes: Joi.string(),
+  last_visit: Joi.date().iso(),
+  next_visit: Joi.date().iso(),
+  notes: Joi.string().allow(""),
   active: Joi.boolean().default(true),
 });
 
