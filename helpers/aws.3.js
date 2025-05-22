c5a89cd8ec2444d55bd08e524a11b3e428e2ba27
sadeeq <sadeequk8@gmail.com>
2025-05-22 17:32:08 +0500

    📌 added

--- helpers/aws.3.js
+++ helpers/aws.3.js
@@ -7,8 +7,8 @@ const { ALLOWED_FILE_TYPES } = require("../constants");
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
   },
 });
 
--- services/cognito.service.js
+++ services/cognito.service.js
@@ -8,8 +8,8 @@ const {
 const cognito = new CognitoIdentityProviderClient({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
   },
 });
 
