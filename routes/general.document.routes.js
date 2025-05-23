a83f5d72e6fdfb604c113ea961318f17e9fa6e23
sadeeq <sadeequk8@gmail.com>
2025-05-23 20:48:34 +0500

    📌 added

--- routes/general.document.routes.js
+++ routes/general.document.routes.js
@@ -7,5 +7,6 @@ router.post("/", uploadFile, generalDocumentController.document_post);
 router.get("/", generalDocumentController.documents_get);
 router.get("/:documentId", generalDocumentController.document_get);
 router.delete("/:documentId", deleteFile, generalDocumentController.document_delete);
+// router.delete("/:documentId", deleteFile, generalDocumentController.document_delete);
 
 module.exports = router;
