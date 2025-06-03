a6ee5718ce9a4ecda921c98c869c664ba0757ae1
sadeeq <sadeequk8@gmail.com>
2025-06-03 17:57:17 +0500

    📌 added

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -6,8 +6,8 @@ const subscriptionController = require("../controllers/subscription.controller")
 router.post("/checkout-session", verifyCognitoToken, subscriptionController.checkout_session_post);
 router.post("/webhook", subscriptionController.handleWebhook);
 router.get("/session/:sessionId", verifyCognitoToken, subscriptionController.session_details_get);
-router.get("/:subscriptionId", verifyCognitoToken, subscriptionController.download_invoice_get);
 router.get("/history", verifyCognitoToken, subscriptionController.subscription_history_get);
+router.get("/:subscriptionId", verifyCognitoToken, subscriptionController.download_invoice_get);
 router.post("/cancel", verifyCognitoToken, subscriptionController.cancel_subscription_post);
 
 module.exports = router;
